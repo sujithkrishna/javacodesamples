3a2909e00f2759885cf95556b2d162f75bebc6bc
10714909 <10714909@LTILBLR20533268.lntinfotech.com>
2022-12-05 05:22:45 +0530

    Java Thread Samples

---
+++ SujithThreadSamples/src/ThreadYieldSamples.java
@@ -0,0 +1,24 @@
+class SampleThread extends Thread{
+	
+	public void run() {
+		for(int i=0;i<10;i++) {
+			System.out.println("Child thread");
+			Thread.yield();
+		}
+	}
+}
+
+public class ThreadYieldSamples {
+
+	public static void main(String[] args) {
+
+		SampleThread samThread = new SampleThread();
+		samThread.start();
+		
+		for(int i=0;i<10;i++) {
+			System.out.println("its Main Thread");
+		}
+	
+	}
+
+}
