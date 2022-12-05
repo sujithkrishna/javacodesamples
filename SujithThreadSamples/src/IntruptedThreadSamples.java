92bb4d28329eac09bb3209c870ad0ca95b6cf58b
10714909 <10714909@LTILBLR20533268.lntinfotech.com>
2022-12-05 07:23:51 +0530

    Thread.Interrupted();

---
+++ SujithThreadSamples/src/IntruptedThreadSamples.java
@@ -0,0 +1,27 @@
+
+class SubThread extends Thread{
+	
+	public void run() {
+		try {
+		for(int i=0;i<10;i++) {
+			System.out.println("I'm sub thread");
+				Thread.sleep(1000);
+			}
+		} catch (InterruptedException e) {
+			System.out.println("I got Interrupted Exception...");
+		}
+	}
+}
+
+public class IntruptedThreadSamples {
+
+	public static void main(String[] args) {
+		System.out.println("Main Thread started");
+		SubThread obj = new SubThread();
+		obj.start();
+		
+		obj.interrupt();
+		System.out.println("Main Thread completed...");
+	}
+
+}
