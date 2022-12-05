048a06deb1702fb1039c37004525225f1552d0e1
SESA695926 <SESA695926@host.docker.internal>
2022-12-06 03:03:44 +0530

    updated code

--- SujithThreadSamples/src/ThreadWaitNotifiy.java
+++ SujithThreadSamples/src/ThreadWaitNotifiy.java
@@ -3,14 +3,18 @@ class SumofThreadExamplesWait extends Thread {
 	public int sum = 0;
 
 	public void run() {
-
-		for (int i = 1; i <= 100; i++) {
-			sum += i;
-		}
-		System.out.println("Output is ready.....");
-		System.out.println("So many Number of lines needs to execute...");
 		synchronized (this) {
+			for (int i = 1; i <= 100; i++) {
+				sum += i;
+			}
+			System.out.println("Output is ready.....");
 			this.notify();
+			System.out.println("So many Number of lines needs to execute...");
+			try {
+				Thread.sleep(2000);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
 		}
 		System.out.println("Lines are executed now...");
 
