6866ee0c37cd8ba38c87eed5af020662594e566a
10714909 <10714909@LTILBLR20533268.lntinfotech.com>
2022-12-05 06:43:09 +0530

    Thread Waiting each other.

---
+++ SujithThreadSamples/src/ChildThreadWaiting.java
@@ -0,0 +1,35 @@
+class ChildClass extends Thread {
+	public void run() {
+		for (int i = 0; i < 10; i++) {
+			System.out.println("Child Class");
+			try {
+				Thread.sleep(1000);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+}
+
+public class ChildThreadWaiting {
+
+	public static void main(String[] args) throws InterruptedException {
+
+		ChildClass cThread= new ChildClass();
+		cThread.start();
+		
+		// Main thread is waiting for child class to complete...
+		cThread.join();
+		
+		
+		for(int i=0;i<10;i++) {
+			System.out.println("Main Class");
+			try {
+				Thread.sleep(1000);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+
+}
---
+++ SujithThreadSamples/src/MainThreadWaiting.java
@@ -0,0 +1,41 @@
+class ChildClass1 extends Thread {
+	
+	public static Thread mainThread;
+	
+	public void run() {
+		try {
+			mainThread.join();
+		} catch (InterruptedException e1) {
+			e1.printStackTrace();
+		}
+		for (int i = 0; i < 10; i++) {
+			System.out.println("Child Class");
+			try {
+				Thread.sleep(1000);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+}
+public class MainThreadWaiting {
+
+	public static void main(String[] args) {
+		
+		ChildClass1.mainThread = Thread.currentThread();
+		
+		ChildClass1 cThread= new ChildClass1();
+		cThread.start();
+		
+		for(int i=0;i<10;i++) {
+			System.out.println("Main Class");
+			try {
+				Thread.sleep(1000);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+
+	}
+
+}
